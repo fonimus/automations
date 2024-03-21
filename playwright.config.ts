4d86171679e40dff6485b32f75d3b660839dd67d
François Onimus <francois.onimus@gmail.com>
2024-03-21 17:42:18 +0100

    fix: locale

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,25 +25,29 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    colorScheme: 'dark',
+    locale: 'fr-FR',
+    timezoneId: 'Europe/Paris',
+
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium fr',
-      use: { ...devices['Desktop Chrome'], locale: 'fr-FR', },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
   ],
 
