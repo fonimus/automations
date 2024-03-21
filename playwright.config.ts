2c62675a97664a0e40167a2f365f721b8b7c4d45
François Onimus <francois.onimus@gmail.com>
2024-03-21 17:12:37 +0100

    fix: package-lock

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ require('dotenv').config();
 export default defineConfig({
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 50 * 1000,
+  timeout: 10 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
