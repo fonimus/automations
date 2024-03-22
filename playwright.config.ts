af007461a9db6eac64bc2c6852ab5258a9a0c276
François Onimus <francois.onimus@gmail.com>
2024-03-22 14:16:44 +0100

    fix: cookies

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,6 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     colorScheme: 'dark',
-    locale: 'en-US',
 
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
